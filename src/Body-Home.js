3c9ba333da63698f05778c73749ff7f7ee915adf
Gavin Maynard <gmay8729@gmail.com>
2023-05-14 13:54:51 -0500

    Small Update

--- src/Body-Home.js
+++ src/Body-Home.js
@@ -5,7 +5,7 @@ function body() {
         <p>Welcome to my website! There is not much here now, but I am working on that.</p>
 
         <p>In the meantime, check out my
-            <a href="https://www.github.com/GavinMaynard17" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://www.github.com/GavinMaynard17" target="_blank" rel="noopener noreferrer"> GitHub</a>
         </p>
     </div>
 }
