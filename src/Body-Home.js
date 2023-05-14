983f0a9a46f47625da3c2d78bc22df60da359b87
Gavin Maynard <gmay8729@gmail.com>
2023-05-14 14:17:38 -0500

    added linkedin link

--- src/Body-Home.js
+++ src/Body-Home.js
@@ -5,6 +5,7 @@ function body() {
         <p>Welcome to my website! There is not much here now, but I am working on that.</p>
 
         <p>In the meantime, check out my <a href="https://www.github.com/GavinMaynard17" target="_blank" rel="noopener noreferrer">GitHub</a>
+            or my <a href="https://www.linkedin.com/in/gavin-maynard-59564a240/" target="_blank" rel="noopener noreferrer">LinkedIn</a>.
         </p>
     </div>
 }
